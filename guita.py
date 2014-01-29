3c9d1e0af87ea097fd6271f9f44d9308b23b62ac
Leandro Ardissone <lardissone@gmail.com>
2014-01-29 15:56:03 -0300

    Now using Ambito Financiero data for guita module

--- guita.py
+++ guita.py
@@ -13,45 +13,65 @@ Requires: requests
 import re
 import requests
 import willie
+from bs4 import BeautifulSoup
 
 
-def _dolar_blue(jenni):
+def _dolar_blue(bot):
     """get blue dollar exchange in ARS"""
 
-    r = requests.get('http://www.eldolarblue.net/getDolarBlue.php?as=json')
+    r = requests.get('http://www.ambito.com/economia/mercados/monedas/dolar/')
     if r.status_code == 200:
-        # assholes didn't formatted JSON well
-        regex = re.compile("exchangerate:\\{buy:(.{0,}),sell:(.{0,})\\}\\}")
-        matchArray = regex.findall(r.text)
-        jenni.say('[DOLAR BLUE]: compra: $%s - venta: $%s' % (matchArray[0][0], matchArray[0][1]))
-    else:
-        jenni.reply('sorry vieja, no me anda el we\'servis del blue...')
+        soup = BeautifulSoup(r.text)
 
-def _dolar(jenni):
+        soup = soup.find_all('div', class_='columna3')[0]
+        blue = soup.find_all('big')
+
+        text = '[DOLAR BLUE]: compra $%s - venta $%s (actualizado: %s - variacion: %s)' % (
+            blue[1].string,
+            blue[2].string,
+            blue[3].string,
+            blue[0].string
+        )
+        bot.say(text)
+    else:
+        bot.reply('sorry vieja, no me anda el we\'servis del blue...')
+
+def _dolar(bot):
     """get dollar exchange in ARS"""
 
-    r = requests.get('http://www.eldolarblue.net/getDolarLibre.php?as=json')
+    r = requests.get('http://www.ambito.com/economia/mercados/monedas/dolar/')
     if r.status_code == 200:
-        # assholes didn't formatted JSON well
-        regex = re.compile("exchangerate:\\{buy:(.{0,}),sell:(.{0,})\\}\\}")
-        matchArray = regex.findall(r.text)
-        jenni.say('[DOLAR]: compra: $%s - venta: $%s' % (matchArray[0][0], matchArray[0][1]))
+        soup = BeautifulSoup(r.text)
+
+        soup = soup.find_all('div', class_='columna1')[0]
+        print soup
+        dolar = soup.find_all('big')
+
+        print dolar
+
+        text = '[DOLAR]: compra $%s - venta $%s (actualizado: %s - variacion: %s)' % (
+            dolar[1].string,
+            dolar[2].string,
+            dolar[3].string,
+            dolar[0].string
+        )
+        bot.say(text)
     else:
-        jenni.reply('sorry vieja, no me anda el we\'servis del dolar...')
+        bot.reply('sorry vieja, no me anda el we\'servis del dolar...')
 
 
-def _bitcoin(jenni):
+def _bitcoin(bot):
     """get bitcoin exchange in USD"""
 
     r = requests.get('https://blockchain.info/es/ticker')
     if r.status_code == 200:
         data = r.json()
-        jenni.say('[BITCOIN]: compra: USD %s - venta: USD %s' % (
+        bot.say('[BITCOIN]: compra: USD %s - venta: USD %s' % (
             data['USD']['buy'],
             data['USD']['sell'],
         ))
     else:
-        jenni.reply('sorry vieja, no me anda el we\'servis del bitcoin...')
+        bot.reply('sorry vieja, no me anda el we\'servis del bitcoin...')
 
 @willie.module.commands('guita')
 def guita(bot, input):
